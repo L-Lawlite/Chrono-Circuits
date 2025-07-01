78bf79d8483212283e0b56ca2f406854ccb1b09b
Lawliet <junirobert21@gmail.com>
2025-07-01 15:26:32 +0530

    Removed the bus from events

--- src/main/java/net/lawliet/chrono_circuits/datagen/ServerDatagen.java
+++ src/main/java/net/lawliet/chrono_circuits/datagen/ServerDatagen.java
@@ -7,7 +7,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import org.slf4j.Logger;
 
-@EventBusSubscriber(modid = ChronoCircuits.MODID, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = ChronoCircuits.MODID)
 public class ServerDatagen {
     private static final Logger LOGGER = LogUtils.getLogger();
 
