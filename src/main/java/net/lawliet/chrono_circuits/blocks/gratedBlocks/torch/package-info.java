f401991f18b85daae16ccc4f5f89f397c7f62b9d
Lawliet <junirobert21@gmail.com>
2025-07-22 17:28:05 +0530

    Completed comparator and torch without datagen

---
+++ src/main/java/net/lawliet/chrono_circuits/blocks/gratedBlocks/torch/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package net.lawliet.chrono_circuits.blocks.gratedBlocks.torch;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
